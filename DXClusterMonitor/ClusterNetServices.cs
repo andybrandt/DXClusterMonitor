b9c3b9d095d27a34636eeb608683e2ae2da02eb8
Andy Brandt <andy@andybrandt.net>
2016-12-14 16:19:02 +0100

    First real commit - app sekelton

---
+++ DXClusterMonitor/ClusterNetServices.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+
+namespace DXClusterMonitor
+{
+
+    public class ClusterNetServices
+    {
+        public Queue<Spot> netLoadSpotsFromApi()
+        {
+            Queue<Spot> currentSpots = new Queue<Spot>();
+            using (WebClient mywebClient = new WebClient())
+            {
+                // ServicePointManager.ServerCertificateValidationCallback += ValidateRemoteCertificate;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
+
+                var spotsInString = mywebClient.DownloadString("http://www.dxcluster.co.uk/index.php?/api/all");
+            }
+
+            return currentSpots;
+        }
+    }
+
+}
---
+++ DXClusterMonitor/SpotsSet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DXClusterMonitor
+{
+    public class Spot
+    {
+        public int?    spotUniqueNumber { get; set; }
+        public string  reportedBy { get; set; }
+        public float?   dxFrequency { get; set; }
+        public class singleDXCallsign
+        {
+            public string dxCallsign { get; set; }
+            public string dxPrefix { get; set; }
+            public string dxCountryName { get; set; }
+            public string dxCQZone { get; set; }
+            public string dxITUZone { get; set; }
+            public string dxContinent { get; set; }
+            public string dxLattitude { get; set; }
+            public string dxLongitude { get; set; }
+        }
+        
+        public string   reporterComment { get; set; }
+        
+
+        //  Example spot from JSON https://www.dxcluster.co.uk/index.php?/api/all  
+        //  {"nr":"16862518","call":"N7EKD","freq":"7165","dxcall":"YC3DY","comment":"= CQ DX = strong w cost",
+        //  "time":"2016-12-14 14:38:00","band":"40","dx_prefix":"YC","dx_name":"INDONESIA","dx_cqz":"28","dx_ituz":"0",
+        //  "dx_cont":"OC","dx_long":"106.8","dx_lat":"-6.2","spotter_prefix":"N",
+        //  "spotter_name":"UNITED STATES OF AMERICA","spotter_cqz":"5","spotter_ituz":"0",
+        //  "spotter_cont":"NA","spotter_long":"-87.9","spotter_lat":"43","mytime":"14\/12\/16 at 14:38"}
+
+    }
+}
